6f969d59e8ca16084aeee12fb8c55055d059976a
Leo Martire <30411754+leomartire@users.noreply.github.com>
2021-05-12 09:46:16 -0700

    update step2

--- step2_compute_field.py
+++ step2_compute_field.py
@@ -22,21 +22,22 @@ from copy import deepcopy
 import time
 import numpy as np
 # from itertools import repeat
-
-useMultiProc = False
+from utils import str2bool
 
 # def kek(Green_RW_, mech):
 #   Green_RW = deepcopy(Green_RW_)
 #   Green_RW.set_mechanism(mech)
 #   print(Green_RW)
 
-def worker(output_folder, Green_RW_, options, mechanism, i, param_atmos):
+def generateAndSaveRWField(parallelSources, output_folder, Green_RW_, options, mechanism, i, param_atmos):
   Green_RW = deepcopy(Green_RW_) # copy because there is only one instance of this variable
   # param_atmos = deepcopy(param_atmos_)
   # options = deepcopy(options_)
   
-  if((not useMultiProc) or (useMultiProc and i==0)):
-    verbose = False
+  if(parallelSources<=1 or (parallelSources>=2 and i==0)):
+    # Print only if we are running serial (parallelSources<=1),
+    #         or only the first case if we are running parallel (parallelSources>=2).
+    verbose = True
   else:
     verbose = False
   # Compute Rayleigh wave field.
@@ -59,6 +60,14 @@ def main():
                       help='Path to last set of options Pickle.')
   required.add_argument('--green', required=True,
                       help='Path to Green functions'' object Pickle.')
+  
+  required.add_argument('--parallel', required=True, type=int, nargs=2,
+                      help='Hybrid parallelisation parameters. '+
+                           'First integer encodes the MPI parallelisation for sources (parameter sourceIDs). '+
+                             '0 = serial; n>0 = MPI over n nodes. '
+                           'Second integer encodes the multithreading parallelisation for the RW modes (as specified in the output of step 1). '+
+                             '0 = serial (not recommended); n>0 = multithread over n CPUs.')
+  
   required.add_argument('--sourceIDs', required=True, type=int, nargs='+',
                       help='IDs of the sources to be imported. Must match files under format ''source_#####_in.pkl'' in the chosen output folder path, generated using the script ''makeSource.py''.')
   required.add_argument('--latminlatmaxlonminlonmax', required=True, type=float, nargs=4,
@@ -82,6 +91,14 @@ def main():
   GreenRWPath = args.green
   atmosPath = args.atmosphere
   sourceIDs = args.sourceIDs
+  
+  parallel = args.parallel
+  if(parallel[0]<0 or parallel[1]<0):
+    sys.exit('[%s, ERROR] Cannot use a negative value for the parallel parameter.' % (sys._getframe().f_code.co_name))
+  if(parallel[0]>len(sourceIDs)):
+    sys.exit('[%s, ERROR] Not a good idea to request more nodes (%d) than there are sources (%d), please fix.' % (sys._getframe().f_code.co_name, parallel[0], len(sourceIDs)))
+  if(parallel[0]==1 and len(sourceIDs)>1):
+    print('[%s, WARNING] Requesting 1 node for multiple (%d) sources. Will run the source loop in serial. This is not optimal but will work.' % (sys._getframe().f_code.co_name, len(sourceIDs)))
   
   options_step2 = {}
   # HERE LOAD NEW OPTIONS FROM ARGS.
@@ -173,19 +190,27 @@ def main():
   velocity_models.plot_atmosphere_and_seismic_fromAtmosFile(output_path, Green_RW.seismic, atmosPath, options['dimension'])
   
   # t1 = time.time()
-  if(useMultiProc):
-    npool = min([np.floor(len(mechanisms_data)/3), 16]) # min 3 mech per proc, max 16 proc
+  if(parallel[0]<=1):
+    # User set parallel[0] = 0 (explicitely serial) or parallel[0] = 1 (explicitely ask for one node). Simply run in serial, do not ask MPI ressources.
+    # Note this consitutes 2 different cases:
+    # - Locally, it is best to set parallel[0] = 0 to explicitely state we want a simple serial simulation.
+    # - On a cluster, it is best to set parallel[0] = 1 since a request to reserve one node need be made explicit, even if we end up running serial on it.
+    for i in range(len(mechanisms_data)):
+      generateAndSaveRWField(parallel[0], output_folders[i], Green_RW, options, mechanisms_data.loc[i], i, param_atmos)
+    
+  elif(parallel[0]>=2):
     print(' ')
-    print('[%s, WARNING] Using multiprocessing to parallelise the computation of the fields over every mechanism.' % (sys._getframe().f_code.co_name))
-    print('[%s, INFO] Spawning %d processes to take care of the %d mechanisms.' % (sys._getframe().f_code.co_name, npool, len(mechanisms_data)))
+    print('[%s, WARNING] Using MPI to parallelise the computation of the fields over every mechanism.' % (sys._getframe().f_code.co_name))
+    print('[%s, INFO] Spawning %d processes to take care of the %d mechanisms.' % (sys._getframe().f_code.co_name, parallel[0], len(mechanisms_data)))
     print('[%s, INFO] We only log the output for the first process, so as not to flood the log.' % (sys._getframe().f_code.co_name))
     print(' ')
-    pool = Pool(npool)
+    sys.exit('[%s, ERROR] NOT IMPLEMENTED.' % (sys._getframe().f_code.co_name))
+    pool = Pool(parallel[0])
     
     # Apply method. Theoretically will chug RAM more because there's only one instance of Green_RW, options, and param_atmos.
     for i in range(len(mechanisms_data)):
       # result = pool.apply_async(worker, (output_folders[i], Green_RW, options, mechanisms_data.loc[i], i, param_atmos))
-      pool.apply(worker, (output_folders[i], Green_RW, options, mechanisms_data.loc[i], i, param_atmos))
+      pool.apply(generateAndSaveRWField, (parallel[0], output_folders[i], Green_RW, options, mechanisms_data.loc[i], i, param_atmos))
     
     # # Starmap method. Added pre-duplication of variables to try and limit RAM chugging; little to no improvement.
     # Green_RWs = []
@@ -210,10 +235,9 @@ def main():
     pool.close()
     pool.join()
     # field = result.get() # Gets returned value for last finished process.
+    
   else:
-    for i in range(len(mechanisms_data)):
-      # field = worker(output_folders[i], Green_RW, options, mechanisms_data.loc[i], i, param_atmos)
-      worker(output_folders[i], Green_RW, options, mechanisms_data.loc[i], i, param_atmos)
+    sys.exit('[%s, ERROR] We shouldn\'t end up here.' % (sys._getframe().f_code.co_name))
   # print(time.time()-t1)
 
 if __name__ == '__main__':
